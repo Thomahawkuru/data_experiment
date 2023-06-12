cf8c21db86fa3e8fda7345c4d3263edd5eaf46d1
Thomahawkuru <thomas.boer.rc@gmail.com>
2023-06-12 16:22:45 +0200

    uploaded data and small changes

--- main.py
+++ main.py
@@ -12,22 +12,22 @@ import readers
 import calculators
 
 # Import csv files --------------------------------------------------------------------------------------------------
-datapath        = os.getcwd() + "/data/"        # full path to data
-savepath        = os.getcwd() + "/save/"        # full path to data
-participants    = 26                            # number of participants
+datapath        = os.getcwd() + "\\data\\"        # full path to data
+savepath        = os.getcwd() + "\\save\\"        # full path to data
+participants    = 30                            # number of participants
 trials          = 2                             # trials per participant
 filename        = 'Experiment.csv'              # looks for files with this string in the name
 
-for participant in range(1, participants + 1):
-    print(), print(), print('Analyisis results for participant {}'.format(participant))
+for participant in range(5, participants + 1):
+    print(), print(), print(f'Analyisis results for participant {participant}')
 
     for trial in range(1, trials + 1):
         print(), print('Trial ' + str(trial))
         
-        trialpath = datapath + '{}/Scene {}/'.format(participant,trial)     
+        trialpath = datapath + f'{participant}\\Scene {trial}'
         
         experiment_data     = readers.file(trialpath, participant, trial, 'Experiment.csv' )
-        timestamps          = calculators.timestamps(experiment_data, savepath, participant, trail)   
+        timestamps          = calculators.timestamps(experiment_data, savepath, participant, trial)   
               
         #hand_data           = readers.file(trialpath, participant, trial, 'Hand.csv' )
         #input_paths         = calcualtors.input_paths(timestamps, hand_data)
